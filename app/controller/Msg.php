2f114a04360477c643c3f898a37471937d5f83df
nascencerice <138132454+nascencerice@users.noreply.github.com>
2023-07-02 08:26:53 +0000

    fix

--- app/controller/Msg.php
+++ app/controller/Msg.php
@@ -71,7 +71,7 @@ class Msg
         $res = Request::param(false);
         Log::write('收到的参数: '.json_encode($res));
 
-        $url = "http://107.173.168.46/echo/".url_encode($res['Content']);
+        $url = "http://107.173.168.46/echo/".urlencode($res['Content']);
         $ret = $this->cg($url );
         $ret = json_decode($ret, true);
         
